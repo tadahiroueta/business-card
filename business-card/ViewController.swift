24c8dfe9cdbcf84340088c9a897af21f649ce643
tadahiroueta <tadahiroueta@gmail.com>
2023-10-17 09:39:10 -0500

    Initial Commit

---
+++ business-card/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  business-card
+//
+//  Created by Ueta, Lucas T on 10/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
